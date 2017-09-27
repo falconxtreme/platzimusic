e8627bb7c806da9e64517c50a5cf7856a52d8069
falconxtreme <falcon.donovan@gmail.com>
2017-09-27 00:04:23 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 {{ title }}
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+    
+
+
+</template>
+
+<script>
+import Artist from './components/Artist.vue';
+import Spinner from './components/Spinner.vue';
+import getArtists from './api';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Platzimusic',
+      artists: [],
+      countries:[
+        {name:'Argentina', value:'argentina'},
+        {name:'Peru', value:'peru'},
+        {name:'Colombia', value:'colombia'},
+        {name:'España', value:'spain'}
+      ],
+      selectedCountry: 'peru', 
+      loading: true
+    }
+  },
+  components: {
+    Artist,
+    Spinner
+  },
+  methods:{
+    refreshArtists(){
+      const self = this;
+      self.loading = true;
+      getArtists(this.selectedCountry)
+        .then((artists) =>{
+          self.artists = artists;
+          self.loading = false;
+        });
+    }
+  },
+  mounted: function(){
+    this.refreshArtists();
+  },
+  watch:{
+    selectedCountry: function(){
+      this.refreshArtists();
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default{
+	appname: 'platzimusic',
+	apiKey: 'c9fabce62620eefb53a3c6656ec8f861',
+	secret: '8ec685cb0d722cabfd09d844979da0bd',
+	registeredTo: 'falcondonovan'
+}
+
+//Application name	platzimusic
+//API key	c9fabce62620eefb53a3c6656ec8f861
+//Shared secret	8ec685cb0d722cabfd09d844979da0bd
+//Registered to	falcondonovan
